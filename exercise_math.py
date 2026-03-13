f055825ee90eebc31656b7c2e1c75dfa471e636e
guadamedina-19 <mgmedinarusso@mail.austral.edu.ar>
2026-03-13 09:02:32 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -13,3 +13,17 @@ def math():
     """
     a = 57
     b = 7
+    suma = a + b
+    print(suma)
+    diferencia = a - b
+    print(diferencia)
+    producto = a * b
+    print(producto)
+    promedio = (a + b) / 2
+    print(promedio)
+    cociente_entero = a// b
+    print(cociente_entero)
+    resto = a % b
+    print(resto)
+    valor_real = a/b
+    print(valor_real)
